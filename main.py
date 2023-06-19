850a4c9dfefcf70e91dc0150cdc521d3d9273d09
Dan <lordaniel24@gmail.com>
2023-06-19 15:30:17 -0500

    basic set up

--- main.py
+++ main.py
@@ -5,8 +5,20 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+@client.event
+async def on_ready():
+    print("Bot is ready")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
 
 
 
 if __name__ == '__main__':
-    pass
+    token = open('secrets/token.txt', 'r').read()
+    client.run(token)
