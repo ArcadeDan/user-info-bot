dfec9859c7a53d32751e7d13a382e868775c6464
Dan <lordaniel24@gmail.com>
2023-06-19 15:15:24 -0500

    init

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import discord 
 
-    
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+
 
 if __name__ == '__main__':
     pass
